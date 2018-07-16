0b335aa9beed716e7d22993cf116b19671ea7146
sirbobz <bobcertified@gmail.com>
2018-07-16 16:51:24 +0300

    initial commit

---
+++ src/main/java/world/jumo/AirtimeServiceApplication.java
@@ -0,0 +1,12 @@
+package world.jumo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirtimeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirtimeServiceApplication.class, args);
+	}
+}
