73a1781182532803b1b7dc1a60d87fc07277a1a0
sirbobz <bobcertified@gmail.com>
2018-07-16 19:28:06 +0300

    updated the dbpller class to recieve the polled data

--- src/main/java/world/jumo/DbPoller/DbPoller.java
+++ src/main/java/world/jumo/DbPoller/DbPoller.java
@@ -4,10 +4,40 @@
  */
 package world.jumo.DbPoller;
 
+import java.util.List;
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import world.jumo.Service.ProcessRequest;
+
+
 /**
  * @author Bob Mwenda
  *
  */
 public class DbPoller {
+	
+	private final Logger logger = LogManager.getLogger(DbPoller.class);
+	
+	public ProcessRequest processRequest;
 
+	public void handleJdbcMessage(List<Map> message) {
+		try 
+			{
+				for (Map<?, ?> resultMap : message) 
+				{					
+					logger.info(resultMap);
+					
+					processRequest.SendRequest(resultMap);
+				}
+			} 
+		catch (Exception e) 
+			{
+				logger.error(e);
+				// UPdate DB for the failed transaction
+			}
+
+	}
 }
---
+++ src/main/java/world/jumo/Service/ProcessRequest.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package world.jumo.Service;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author dibon
+ *
+ */
+@Service
+public class ProcessRequest {
+
+	public void SendRequest(Map<?, ?> resultMap) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
