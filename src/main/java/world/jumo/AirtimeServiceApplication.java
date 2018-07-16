7e89569db374cca544f4a9fc3b07c44b5ae7b22f
sirbobz <bobcertified@gmail.com>
2018-07-16 19:41:49 +0300

    updated the main class to include resources and scanned packages

--- src/main/java/world/jumo/AirtimeServiceApplication.java
+++ src/main/java/world/jumo/AirtimeServiceApplication.java
@@ -1,9 +1,14 @@
 package world.jumo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.context.annotation.ImportResource;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "world.jumo")
+@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
+@ImportResource("JdbcPoller.xml")
 public class AirtimeServiceApplication {
 
 	public static void main(String[] args) {
