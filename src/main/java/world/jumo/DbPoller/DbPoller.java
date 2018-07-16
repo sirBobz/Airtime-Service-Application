266ffb95a54d1d4663a7585b997148a4b75c123a
sirbobz <bobcertified@gmail.com>
2018-07-16 17:33:13 +0300

    created the dbpoller class

---
+++ src/main/java/world/jumo/DbPoller/DbPoller.java
@@ -0,0 +1,13 @@
+/**
+ * This poller class receives the data from the database 
+ * and routes it to the processing service
+ */
+package world.jumo.DbPoller;
+
+/**
+ * @author Bob Mwenda
+ *
+ */
+public class DbPoller {
+
+}
