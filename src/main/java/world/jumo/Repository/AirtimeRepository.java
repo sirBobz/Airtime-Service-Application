033e163df96866b6ef3f781093cac50236e5f0f7
sirbobz <bobcertified@gmail.com>
2018-07-16 17:31:28 +0300

    Updated the repository

--- src/main/java/world/jumo/Repository/AirtimeRepository.java
+++ src/main/java/world/jumo/Repository/AirtimeRepository.java
@@ -1,12 +1,19 @@
 /**
- * 
+ * This is the repository to access the class object via ORM
  */
 package world.jumo.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import world.jumo.Model.AirtimeModel;
+
 
 /**
  * @author Bob Mwenda
  *
  */
-public class AirtimeRepository {
+@Repository
+public interface AirtimeRepository extends JpaRepository<AirtimeModel, Long> {
 
 }
